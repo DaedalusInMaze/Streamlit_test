9b4c98580e0e3f85951cb81f6e61aeebb6a34e39
Curtis Zhuang <60707834+DaedalusInMaze@users.noreply.github.com>
2023-06-14 10:57:00 -0400

    Update streamlit_test.py

--- streamlit_test.py
+++ streamlit_test.py
@@ -3,6 +3,7 @@ import pandas as pd
 import numpy as np  
 from ydata_profiling import ProfileReport
 import streamlit as st
+from streamlit_pandas_profiling import st_profile_report
 import streamlit.components.v1 as components
 import eda_tools.missing_analysis as ma
 import eda_tools.EDA_tool as eda
@@ -12,5 +13,5 @@ st.dataframe(df)
 
 # Profile Report
 profile = ProfileReport(df, explorative=False, config_file="profile_config/yprofile_config_default.yaml")
-#st_profile_report(profile)
-components.html(profile.to_notebook_iframe())
+st_profile_report(profile)
+#components.html(profile.to_notebook_iframe())
